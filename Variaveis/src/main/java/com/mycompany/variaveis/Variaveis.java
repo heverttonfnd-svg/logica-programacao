de5748658a85673c9affa550f9cbea5f103e783f
Hevertton Silva <hevertton67191686@alunos.to.senac.br>
2026-09-02 17:56:13 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author HeverttonSilva
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!eu sou Hevertton");
+    }
+}
