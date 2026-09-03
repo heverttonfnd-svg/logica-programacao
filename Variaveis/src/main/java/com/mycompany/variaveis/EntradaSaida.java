2f57cbee3045501945f02b34b057a1d2ac455ab4
Hevertton Silva <hevertton67191686@alunos.to.senac.br>
2026-09-03 17:11:13 -0300

    entradasaida de dados

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/EntradaSaida.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author HeverttonSilva
+ */
+//importanto as bibliotecas do Scanner
+
+import java.util.Scanner;
+
+public class EntradaSaida {
+
+public static void main(String[] args)  {
+   
+    // codigo para cadastrar o pet
+    // variaveis declasadas sem dados
+    String nome;
+    String raca;
+    int idade;
+    double peso;
+    boolean foiVacinado;
+    
+    // criando as variaveis de captura de dados
+    Scanner entradaTexto = new Scanner(System.in);
+   Scanner entradaNumero= new Scanner(System.in);
+   Scanner entradaLogica= new Scanner(System.in);
+   
+    System.out.println("Digite o nome do seu prt:"); 
+    nome= entradaTexto.nextLine();
+    
+    System.out.println("Digite a raca do seu pet:");
+    nome= entradaTexto.nextLine();
+    
+    System.out.println("Digite a idade do seu pet:");
+    idade= entradaNumero.nextInt();
+    
+    System.out.println("digite o peso do seu pet:");
+    peso= entradaNumero.nextDouble();
+    
+    System.out.println("seu pet e vacinado?");
+    System.out.println("digite true para sim, ou 0 para nao");
+    foiVacinado= entradaLogica.hasNext();
+    
+    System.out.println("bem-vindo(a) "+nome);
+    
+    
+    
+    
+}//fim da main
+
+}//fim da classe EntradaSaida
--- Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -9,8 +9,26 @@ package com.mycompany.variaveis;
  * @author HeverttonSilva
  */
 public class Variaveis {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!eu sou Hevertton");
-    }
+//A classe main e quem executa os codigos
+    public static void main(String[] args)  {
+        /*codigos de saida
+        - sao utilizados para imprimir/mostrar
+        dados e mensagens.    
+        */ 
+       System.out.println("Ola");
+       System.out.println("eu sou um programadorweb");
+       //println pula linha,print mostra na frente do outro
+       System.out.print("eu ja sei programar");
+       System.out.print(" em java");
+              
+       //CRIACAO DE VARIAVEIS
+       int idade = 15;//valor inteiro
+       double altura = 1.80;//valor decimal grande
+       float peso ; // valor decimal pequeno
+       String nome = "hevertton fernanades";//textos
+       boolean casada = false; //valor logico
+     // true - verdadeiro , false - falso
+        System.out.println(" Ola "+nome);
+        System.out.println("voce tem"+idade);
+    } //fim da main
 }
